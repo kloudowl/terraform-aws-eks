f52c05ed0e42bb953446a0c7066519cba5dc98af
Greg Werner <werner.greg@gmail.com>
2022-12-23 17:25:03 -0500

    feat: Add auth0 resources
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

--- main.tf
+++ main.tf
@@ -1,28 +1,11 @@
 provider "aws" {
-  region = var.region
+  region = var.aws_region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+provider "auth0" {
+  domain        = var.auth_tenant_domain
+  client_id     = var.auth_client_id
+  client_secret = var.auth_client_secret
+  debug         = var.auth_debug
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
-}
--- terraform.tf
+++ terraform.tf
@@ -1,11 +1,12 @@
 terraform {
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.48.0"
     }
+    auth0 = {
+      source = "auth0/auth0"
+      version = "~> 0.41.0"
+    }
   }
-
-  required_version = ">= 1.3.6"
 }
