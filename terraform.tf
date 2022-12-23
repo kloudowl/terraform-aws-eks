b55ec0670e1e9eaaee308c0db1f2dd884fbc60a3
Greg Werner <werner.greg@gmail.com>
2022-12-23 11:09:25 -0500

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,19 +1,11 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
-
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 4.48.0"
     }
   }
 
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.3.6"
 }
